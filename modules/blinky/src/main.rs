c8460914bae1f835912e645c50e80f471c21a5a0
quiescentFET <amandeep.singh1028@gmail.com>
2026-02-18 01:46:15 -0500

    added timer

--- modules/blinky/src/main.rs
+++ modules/blinky/src/main.rs
@@ -4,6 +4,8 @@
 use defmt::info;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
 use panic_probe as _;
 
 #[defmt::panic_handler]
@@ -14,9 +16,14 @@ fn panic() -> ! {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("loading config...");
-    let config = embassy_stm32::Config::default();
+    let p = embassy_stm32::init(Default::default());
+    // let mut config = embassy_stm32::Config::default();
     info!("config loaded!");
+
+    let mut ld2 = Output::new(p.PA5, Level::Low, Speed::Low);
+
     loop {
-        info!("Loop running!");
+        Timer::after_millis(500).await;
+        ld2.toggle();
     }
 }
